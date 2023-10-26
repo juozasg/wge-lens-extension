19a38a2c998fab8ff00988a25a6b5a3cdff88025
Juozas Gaigalas <juozasgaigalas@gmail.com>
2023-10-26 18:20:35 +0300

    example UI

--- renderer.tsx
+++ renderer.tsx
@@ -8,14 +8,17 @@ type KubeObjectMenuProps<T> = Renderer.Component.KubeObjectMenuProps<T>;
 // type Namespace = Renderer.K8sApi.Namespace;
 type KubeObject = Renderer.K8sApi.KubeObject;
 
+// export function ExampleIcon() {
+//   return <Renderer.Component.Icon
+//     material="airport_shuttle"
+//     interactive={toolbar}
+//     title="Open in WGE Portal..."
+//   />
+// }
+
+
 /**
  * 
- * RendererExtension which extends LensRendererExtension runs in Lens' 'renderer' process (NOT 'main' process)
- * main vs renderer <https://www.electronjs.org/docs/tutorial/quick-start#main-and-renderer-processes>
- * 
- * LensRendererExtension is the interface to Lens' renderer process. Its api allows you to access, configure, 
- * and customize Lens data add custom Lens UI elements, and generally run custom code in Lens' renderer process.
- *
  * To see console statements in 'renderer' process, go to the console tab in DevTools in Lens
  * View > Toggle Developer Tools > Console.
  * 
@@ -27,6 +30,26 @@ export default class OciImageExtensionRenderer extends Renderer.LensExtension {
   onActivate() {
     console.log("wge-lens Rendered activated");
   }
+
+  // Examples nested page menus
+  // clusterPageMenus = [
+  //   {
+  //     target: { pageId: "hello" },
+  //     title: "Hello World Parent",
+  //     id: "hello-world-parent",
+  //     components: {
+  //       Icon: ExampleIcon,
+  //     }
+  //   },
+  //   {
+  //     target: { pageId: "hello" },
+  //     title: "Hello World Child",
+  //     parentId: "hello-world-parent",
+  //     components: {
+  //       Icon: ExampleIcon,
+  //     }
+  //   }
+  // ]
 
   kubeObjectMenuItems = [
     {
